d5b47dd3000375ceabdb7c6d58fdbde17a28cdaa
Beatriz Frederico <beatrizfrederico5@gmail.com>
2023-11-15 02:15:39 -0300

    Tela de perfil host iniciada

---
+++ AppExchance/app/src/main/java/com/example/appexchance/TelaPerfilHost.kt
@@ -0,0 +1,11 @@
+package com.example.appexchance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaPerfilHost : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_perfil_host)
+    }
+}
